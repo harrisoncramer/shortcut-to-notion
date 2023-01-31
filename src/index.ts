390d2ddd8e41267f902315ce5d2c92be7a1f5ea6
Harrison Cramer <harrisoncramer@gmail.com>
2023-01-30 19:24:46 -0500

    Just POSTs

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ const PORT = 3000
 const app = express();
 app.use(express.json());
 
-
-app.get("/", async (req, res) => {
-  console.log(req.body)
-  res.send("Hello World")
-})
-
 app.post("/", async (req, res) => {
   console.log(req.body)
   res.send("Post successful")
